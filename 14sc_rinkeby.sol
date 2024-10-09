2a3dc201eacb0520925d5413658187b838fc2396
GlebRokotov <glebrokotov0708@gmail.com>
2024-10-10 00:44:29 +0300

    Add files via upload

---
+++ 14sc_rinkeby.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract Greeter {
+    string greet;
+
+    function setGreet(string memory _greet) public {
+        greet = _greet;
+    }
+
+    function getGreet() public view returns (string memory) {
+        return greet;
+    }
+}
